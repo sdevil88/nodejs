e5ed6f5894c52bd32f899a66897e5f274f75ee06
sagar <wwwghimiresagar88@gmail.com>
2023-08-08 13:51:57 +0545

    sum node js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const http = require("http");
-const date =require("./sum");
-const myfunction = (req,res) =>
-{
-    const DateTime = Date();
-    res.end(`hello world ${DateTime}`);
-};
+// const http = require("http");
+// const date =require("./sum");
+// const myfunction = (req,res) =>
+// {
+//     const DateTime = Date();
+//     res.end(`hello world ${DateTime}`);
+// };
 
-http.createServer(myfunction).listen(8080);
-console.log("app is running on port number : 8080");
+// http.createServer(myfunction).listen(8080);
+// console.log("app is running on port number : 8080");
+
+
+const http = require("http");
+const file =  require("./sum");
+
+const func =(req,res)=>{
+    const sum = run();
+    res.end(`hello ${sum}`);
+}
+http.createServer(func).listen(8080);
+console.log("it is running ");
+
 
--- sum.js
+++ sum.js
@@ -1,3 +1,29 @@
-const date = new Date();
-console.log(date);
-module.exports = date;
+// const date = new Date();
+// console.log(date);
+// module.exports = date;
+
+
+const add = {
+    a:20,
+    b:30,
+    value:()=>{
+        
+        return add;
+    },
+    show:()=>{
+        let a=add.a+add.b;
+        console.log(a);
+    }
+
+};
+function run(){
+    add.value();
+add.show();
+}
+run();
+
+module.exports = run;
+
+
+
+
