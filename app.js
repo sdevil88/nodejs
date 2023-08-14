d78b5ba3b641d4b119a05c28e439e41a878994d7
sagar <wwwghimiresagar88@gmail.com>
2023-08-14 09:05:12 +0545

    email

--- app.js
+++ app.js
@@ -45,6 +45,41 @@
 
 
 
+//weather apt using package axios and open source weather url link d
+
+const axios = require("axios");
+async function getUser(url){
+    try{
+        const { data } = await axios.get(url);
+        const { daily } = data;
+        return { sunrise:daily.sunrise, sunset:daily.sunset };
+    }
+        catch (error) {
+            console.erro(error);
+        } 
+    }
+
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
+
+    async function solve(){
+        const sunrise = await getUser(url)
+        console.log({ sunrise });
+    }
+
+
+    solve();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ emailtemplate.js
@@ -0,0 +1,48 @@
+//email sent in node js
+// plan of actio 
+
+// sent mail to user saying hwllo world 
+
+//send email to multiple user by function 
+
+//email template designed template
+
+// const tfile = require("npm install nodemailer-express-handlebars");
+
+const nodemailer = require("nodemailer");
+const Mail = require("nodemailer/lib/mailer");
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+      user: 'wwwghimiresagar88@gmail.com',
+      pass: 'qdbmhqptosivcdzb'
+    }
+  });
+
+
+
+
+
+
+  async function main(sender) {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"haha" <wwwghimiresagar88@gmail.com>', // sender address
+      to: sender.toString(), // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    
+}
+//   main().catch(console.error);
+
+const sender = ["wwwghimiresagar88@gmail.com", "s.g.devil.88@gmail.com"];
+  main(sender) 
+
